16667c3a22a9938fe3144165e9a133b80dae8aa8
Igor Bykov <igorb.dev@websm.io>
2017-12-04 15:31:19 +0300

    any changes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 const yaml = require('js-yaml');
 const minimist = require('minimist');
 const request = require('request');
 const chalk = require('chalk');
+const WebSocket = require('ws');
 const log = console.log;
 
 const ARGV = minimist(process.argv.slice(2));
@@ -64,28 +66,63 @@ switch (command) {
 
     case 'start':
 
-        request(`${host}:${port}/jails/${configData.name}/log-stream`)
-            .pipe(process.stdout);
+        let addr = url.parse(host);
+        // const ws = new WebSocket(`ws://${addr.host}:${port}/`);
+        const ws = new WebSocket(`ws://${addr.host}:8080/`);
 
-        request({
-            method: 'POST',
-            uri: `${host}:${port}/jails`,
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(configData),
-        }, (error, response, body) => {
+        ws.on('open', _ => {
 
-            console.log(error);
+            console.log('connected');
+            ws.send(configData.name);
+
+            request({
+                method: 'POST',
+                uri: `${host}:${port}/jails`,
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(configData),
+            }, (error, response, body) => {
+
+                console.log(error);
+                // process.exit();
+
+            });
+        });
+
+        ws.on('close', _ => console.log('disconnected'));
+
+        ws.on('message', data => {
+
+            let {
+                level,
+                text
+            } = JSON.parse(data);
+
+            switch (level) {
+
+                case 'notice':
+                    text = chalk.green(text);
+                    break;
+
+                case 'warn':
+                    text = chalk.yellow(text);
+                    break;
+
+                case 'crit':
+                    text = chalk.red(text);
+                    break;
+
+            }
+
+            console.log(text);
 
         });
+
 
         break;
 
     case 'stop':
-
-        request(`${host}:${port}/jails/${configData.name}/log-stream`)
-            .pipe(process.stdout);
 
         request({
             method: 'DELETE',

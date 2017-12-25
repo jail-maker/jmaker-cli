3cd9c1d15ab8e313f3cbc64c8d463f8aa75fbbc3
Igor Bykov <igorb.dev@websm.io>
2017-12-25 17:56:41 +0300

    added globals.js

---
+++ libs/globals.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const ARGV = minimist(process.argv.slice(2));
+
+class Globals {
+
+    constructor() {
+
+        let {
+
+            port = 3346,
+            host = 'http://127.0.0.1',
+            config = 'jmakefile.yml',
+            profile = undefined
+
+        } = ARGV;
+
+        let command = ARGV._[0];
+
+        if (!profile) profile = process.env['JMAKER_PROFILE'];
+
+        this.port = port;
+        this.host = host;
+        this.configFile = config;
+        this.profile = profile;
+        this.command = command;
+        this.cwd = process.cwd();
+
+    }
+
+}
+
+module.exports = new Globals();

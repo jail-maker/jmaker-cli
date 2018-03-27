03bfebb413192b6621572d6b0b34bd0223d25955
Dmitry Marov <d.marov94@gmail.com>
2018-03-27 17:55:42 +0300

    hot fix

---
+++ command/login.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const prequest = require('request-promise-native');
+const base64url = require('base64url');
+const passwordPrompt = require('password-prompt');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const chalk = require('chalk');
+const JailConfig = require('../lib/jail-config.js');
+const LogWebSocket = require('../lib/log-web-socket.js');
+
+exports.command = 'login';
+
+exports.describe = 'aquire token';
+
+exports.builder = yargs => {
+
+    return yargs;
+
+}
+
+exports.handler = args => {
+
+    let jailConfig = new JailConfig(args['jail-config']);
+
+    let {name, password} = await inquirer.prompt([
+        {prefix: '', name: 'name', message: 'name: ', type: 'input'},
+        {prefix: '', name: 'password', message: 'password: ', type: 'password'}
+    ]);
+
+    let res = await prequest({
+        uri: `${args['server-protocol']}://${args['server-socket']}/jails/create`,
+        method: 'GET',
+        'auth': {
+            'user': name,
+            'pass': password,
+            'sendImmediately': false
+        },
+        json: true,
+        body: {
+            image: jailConfig.name,
+            repository: jailConfig.from,
+        }
+    });
+
+    fs.writeFileSync(args['token-file'], JSON.stringify(res));
+
+}

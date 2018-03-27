40e5928759f04b045a6ffe2d4707b93c597fcea3
Dmitry Marov <d.marov94@gmail.com>
2018-03-27 17:38:20 +0300

    hot fix

--- command/push.js
+++ command/push.js
@@ -1,26 +1,53 @@
 'use strict';
 
-const prequest = require('request-promise-native');
-const configData = require('../libs/config-data.js');
-const globals = require('../libs/globals.js');
+const request = require('request');
+const chalk = require('chalk');
+const JailConfig = require('../lib/jail-config.js');
+const LogWebSocket = require('../lib/log-web-socket.js');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = async _ => {
+exports.command = 'push';
 
-    let tokenContent = fs.readFileSync(globals.tokenFile);
-    let tokenJson = JSON.parse(tokenContent);
+exports.describe = 'push to repository';
 
-    await prequest({
-        uri: `${globals.host}:${globals.port}/images/push-to-repo`,
-        method: 'POST',
-        json: true,
-        body: {
-            image: configData.name,
-            repository: configData.from,
-            tokenJson: tokenJson,
-        }
-    });
+exports.builder = yargs => {
+
+    return yargs;
 
 }
 
+exports.handler = args => {
+
+    let jailConfig = new JailConfig(args['jail-config']);
+    let logWebSocket = new LogWebSocket(`${args['log-protocol']}://${args['log-socket']}`, jailConfig);
+
+    // let tokenContent = fs.readFileSync(globals.tokenFile);
+    // let tokenJson = JSON.parse(tokenContent);
+
+    request({
+        method: 'POST',
+        uri: `${args['server-protocol']}://${args['server-socket']}/images/push-to-repo`,
+        json: true,
+        timeout: null,
+        body: {},
+        // body: {
+        //     image: configData.name,
+        //     repository: configData.from,
+        //     tokenJson: tokenJson,
+        // },
+    }, (error, response, body) => {
+
+        let code = response.statusCode;
+
+        if (code !== 200) {
+
+            console.log(chalk.red(`${code} ${body}`));
+
+        }
+
+        logWebSocket.close();
+
+    });
+
+}
